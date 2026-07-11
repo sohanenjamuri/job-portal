122d196ceee17a6e6faa1e2ea96f3dcf878a23b0
sohanenjamuri <sunnyenjamuri4456@gmail.com>
2026-07-11 15:45:11 +0530

    done

--- Job-Portal-main/backend/index.js
+++ Job-Portal-main/backend/index.js
@@ -19,10 +19,21 @@ const __dirname = path.resolve();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+// const corsOptions = {
+//   origin: "http://localhost:5173",
+//   credentials: true,
+// };
+
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://job-portal-one-virid.vercel.app"
+  ],
   credentials: true,
 };
+
+app.use(cors(corsOptions));
+
 app.use(cors(corsOptions));
 
 // routes (api's)
